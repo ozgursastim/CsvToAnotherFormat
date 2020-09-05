1adc5165300902f8431f680997e3811eb1eaa87f
ozgursastim <sastimozgur@gmail.com>
2020-09-06 00:38:36 +0100

    Add flask API code

---
+++ app.py
@@ -0,0 +1,25 @@
+import json
+import pandas as pd
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/get_json', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        provided_data = request.files.get('file')
+        if provided_data is None:
+            return 'Please enter valid excel format', 400
+        data = provided_data
+        df = pd.read_csv(data)
+        transformed = df.to_json()
+
+        result = {
+            'result':transformed,
+        }
+
+        json.dumps(result)
+
+        return result
+if __name__ == '__main__':
+    app.run()
